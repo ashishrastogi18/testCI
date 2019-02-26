4732228f1af2d1062ff7c40be4441d839275cbf6
Ashish Rastogi <ashish.rastogi18@gmail.com>
2019-02-26 12:21:24 -0500

    updating gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,10 @@
 const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
-
+//added for Continous Integration
+require('./gulpfile-deploy-app-package');
+require('./gulpfile-serve-info');
+require('./gulpfile-update-manifest');
+require('./gulpfile-upload-to-sharepoint');
+require('./gulpfile-upload-app-package');
 build.initialize(gulp);
